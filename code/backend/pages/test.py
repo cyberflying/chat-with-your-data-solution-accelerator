809cb1da0ffe5db4984a718b9f09e596624a9ba9
Kevin Yang <cyberflying@hotmail.com>
2024-08-15 21:10:22 +0800

    update test.py image

--- code/backend/pages/test.py
+++ code/backend/pages/test.py
@@ -17,7 +17,7 @@ logger = logging.getLogger(__name__)
 
 st.set_page_config(
     page_title="Test",
-    page_icon=path.join("images", "favicon.ico"),
+    page_icon=path.join("images", "Copilot.png"),
     layout="wide",
     menu_items=None,
 )
